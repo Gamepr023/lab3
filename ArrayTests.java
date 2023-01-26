77006c890cb49c4efd12f69f187039c2327a590e
Gamepr023 <122562465+Gamepr023@users.noreply.github.com>
2023-01-26 11:51:17 -0800

    changes

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] arr = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(arr);
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, arr);
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] arr = {1, 2, 3, 4, 5};
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, ArrayExamples.reversed(arr));
+  }
 }
---
+++ ListTests.java
@@ -0,0 +1,53 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class MyStringChecker implements StringChecker {
+    String string;
+
+    public MyStringChecker(String s) {
+        string = s;
+    }
+
+    public boolean checkString(String s) {
+        if (s.equals(string)) {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class ListTests {
+    @Test
+    public void testFilter() {
+        List<String> list = new ArrayList<>();
+        ArrayList<String> output = new ArrayList<>();
+        StringChecker sc = new MyStringChecker("a");
+        list.add("a");
+        list.add("a");
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        output.add("a");
+        output.add("a");
+        output.add("a");
+        assertEquals(output, ListExamples.filter(list, sc));
+    }
+
+    @Test
+    public void testMerge() {
+        List<String> list1 = new ArrayList<>();
+        list1.add("a");
+        list1.add("c");
+        list1.add("d");
+        List<String> list2 = new ArrayList<>();
+        list2.add("a");
+        list2.add("b");
+        list2.add("d");
+        list2.add("e");
+        list2.add("f");
+        List<String> output = new ArrayList<>();
+        assertEquals(output, ListExamples.merge(list1,list2));
+    }
+}
